f1afbb6f1636612efee10a5fcf9338dd621c77e0
nanisai937 <nanisai937@gmail.com>
2020-08-09 05:19:21 -0400

    Initial Author

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
+const bodyParser = require('body-parser')
+
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
@@ -13,6 +16,9 @@ app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
 app.use('/', indexRouter)
+app.use('/authors', authorRouter)
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
+
 
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL, {
